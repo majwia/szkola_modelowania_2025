ee52b3fc3f08112b66422bb4624cb8a51cce7ad9
Maja Wiącek <147046185+majwia@users.noreply.github.com>
2025-05-23 11:16:10 +0200

    Update and rename chyba_koniec.py to simulation.py

--- simulation.py
+++ simulation.py
@@ -8,25 +8,29 @@ import random
 import traci
 import csv
 
-# ---------- Konfiguracja tras i parkowania ----------
-NET_FILE = 'mapa22.net.xml'
-routes_file = 'parking_full_routes_mapa22.json'
+NET_FILE = 'parking.net.xml'
+routes_file = 'parking_full_routes_test2.json'
 
 with open(routes_file, 'r', encoding='utf-8') as f:
     parking_data = json.load(f)
 
 parking_capacities = {
-    "pa_1": 24, "pa_0": 7, "pa_2": 24, "pa_3": 24, "pa_4": 40,
-    "pa_5": 24, "pa_6": 24, "pa_7": 10, "pa_8": 13, "pa_9": 11,
-    "pa_10": 12, "pa_11": 11, "pa_12": 7
+    "pa_0": 9, "pa_1": 9, "pa_2": 8, "pa_3": 10, "pa_4": 9,
+    "pa_10": 10, "pa_11": 8, "pa_12": 8, "pa_20": 9, "pa_21": 9,
+    "pa_22": 8, "pa_23": 6, "pa_24": 5, "pa_29": 6, "pa_30": 9,
+    "pa_31": 8, "pa_32": 9, "pa_33": 5, "pa_39": 5, "pa_40": 12,
+    "pa_41": 8, "pa_42": 6, "pa_43": 4, "pa_49": 7, "pa_50": 8,
+    "pa_51": 4, "pa_52": 8, "pa_53": 8
 }
 
+# PARKING -> LISTA KRAWEDZIE KTORE DO NIEGO PROWADZA
 parking_area_to_edges = {}
 for edge_id, data in parking_data.items():
     pa = data['parking']
     parking_area_to_edges.setdefault(pa, []).append(edge_id)
 
 
+# DLUGOSC WSZYSTKICH KRAWEDZI
 def get_edge_lengths(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
@@ -41,41 +45,47 @@ def get_edge_lengths(xml_file):
 
 lengths = get_edge_lengths(NET_FILE)
 
-
+# IM WYZSZY NUMER KRAWEDZI TYM MNIEJSZA WAGA
 def get_exp_weights(keys, lambd=0.1):
-    # Im mniejsza krawędź E, tym wyższe prawdopodobieństwo (ujemne lambda)
     values = np.array([int(re.search(r'E(\d+)', k).group(1)) for k in keys])
     weights = np.exp(-lambd * values)
     return weights / weights.sum()
 
 
-def generate_random_routes(seed=None):
+def generate_random_routes(seed = None):
     if seed is not None:
-        random.seed(seed)
         np.random.seed(seed)
-
+    # ILOSC SAMOCHODOW W SYMULACJI
     max_slots = sum(parking_capacities.values())
-    # sim_n = np.random.randint(int(0.7 * max_slots), max_slots + 1)
-    sim_n = int(max_slots*0.5)
-    
+    sim_n = int(max_slots * 0.75)
+
+    # AKTUALNA ILOSC JUZ PRZYPISANYCH SAMOCHODOW DO PARKINGU
     assignments = {pa: 0 for pa in parking_capacities}
     vehicles = []
     arrivals = []
+
     i = 0
     while len(vehicles) < sim_n:
+        
+        # PARKINGI Z WOLNYMI MIEJSCAMI
         available_pas = [pa for pa in parking_capacities if assignments[pa] < parking_capacities[pa]]
         if not available_pas:
             break
-
+        
+        # LOSOWANIE PARKINGU
         pa = random.choice(available_pas)
         edges = parking_area_to_edges[pa]
         weights = get_exp_weights(edges, lambd=0.1)
         edge_id = np.random.choice(edges, p=weights)
 
+        # VIA I PARKING ID
         entry = parking_data[edge_id]
+        # VIA
         via_to_park = entry['via'].split()
+        # LOSOWY PRZYJAZD
         depart = abs(np.random.normal(20, 10))
-        travel_time = sum(lengths.get(e, 0) for e in via_to_park) / 13.89
+        travel_time = sum(lengths.get(e, 0) for e in via_to_park) / 2.89
+        # DOJAZD NA MIEJSCA
         arrival = depart + travel_time
 
         vehicles.append({
@@ -90,55 +100,51 @@ def generate_random_routes(seed=None):
         assignments[pa] += 1
         i += 1
 
+    # KIEDY OSTATNI WJEDZIE NA PARKING
     max_arrival = max(arrivals)
     base_depart = max_arrival + 20.0
 
-    # Sortujemy pojazdy tak, że mniejsze numery krawędzi wyjeżdżają pierwsze
+    # LISTA ID KRAWEDZI
     edge_nums = np.array([int(re.search(r'E(\d+)', v['edge_id']).group(1)) for v in vehicles])
-    sorted_idx = np.argsort(edge_nums)
-    
-    # Możesz też użyć skalowanej rozrzutności czasów:
-    # scales = np.linspace(1.0, 3.0, len(vehicles))
-    # # Stosunkowo krótsze przerwy dla pojazdów z mniejszymi numerami krawędzi
-    edge_nums_sorted = edge_nums[sorted_idx]
-    scales = 2.0 + 0.05 * edge_nums_sorted
-    exp_gaps = np.random.exponential(scale=scales)
+    sorted_idx = np.argsort(-edge_nums)  
+    exp_gaps = np.random.exponential(scale=2.0, size=len(vehicles))
+    sorted_departures = base_depart + np.cumsum(exp_gaps)
 
     veh_output = []
-    for idx, j in enumerate(sorted_idx):
+    for i, j in enumerate(sorted_idx):
         v = vehicles[j]
-        wait = np.random.uniform(500, 700)
+        wait = sorted_departures[i] - v['arrival'] #CZAS POSTOJU NA PARKINGU
         veh_output.append({
-            'id':       v['id'],
-            'depart':   v['depart'],
-            'via':      v['via'],
-            'parking':  v['parking'],
+            'id': v['id'],
+            'depart': v['depart'],
+            'via': v['via'],
+            'parking': v['parking'],
             'duration': wait
         })
 
-    veh_output.sort(key=lambda v: v['depart'])
-    exitRoute = "E19"
+    veh_output.sort(key=lambda v: v['depart']) # POSORTOWANE WEDŁUG DEPART
 
-    with open('mapa22.rou.xml', 'w', encoding='utf-8') as routes:
-        routes.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-        routes.write('<routes xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' \
-                     'xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/routes_file.xsd">\n')
-        routes.write(f'  <route id="exitRoute" edges="{exitRoute}"/>\n')
+    exitRoute = "E14"
+    with open('parking.rou.xml', 'w', encoding='utf-8') as routes:
+        print('<?xml version="1.0" encoding="UTF-8"?>', file=routes)
+        print('<routes xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/routes_file.xsd">', file=routes)
+        print(f'  <route id="exitRoute" edges="{exitRoute}"/>', file=routes)
+
         for v in veh_output:
-            routes.write(f'  <vehicle id="{v["id"]}" depart="{v["depart"]:.2f}">\n')
-            routes.write(f'    <route edges="{v["via"]}"/>\n')
-            routes.write(f'    <stop parkingArea="{v["parking"]}" duration="{v["duration"]:.2f}"/>\n')
-            routes.write('  </vehicle>\n')
-        routes.write('</routes>\n')
+            print(f'  <vehicle id="{v["id"]}" depart="{v["depart"]:.2f}">', file=routes)
+            print(f'    <route edges="{v["via"]}"/>', file=routes)
+            print(f'    <stop parkingArea="{v["parking"]}" duration="{v["duration"]:.2f}"/>', file=routes)
+            print('  </vehicle>', file=routes)
 
-# ---------- Konfiguracja symulacji ----------
+        print('</routes>', file=routes)
+
 SUMO_BINARY = "sumo"
-SUMO_CFG = "mapa22.sumocfg"
+SUMO_CFG = "parking.sumocfg"
 STEP_LENGTH = "0.05"
 DELAY = "1"
 LATERAL_RES = "0"
-N_RUNS = 200
-OUT_DIR = "sim_results_map1_50"
+N_RUNS = 100
+OUT_DIR = "nook1"
 os.makedirs(OUT_DIR, exist_ok=True)
 
 def run_simulation(run_id):
@@ -155,7 +161,6 @@ def run_simulation(run_id):
         traci.simulationStep()
     traci.close()
 
-    # Parsuj wyniki
     tree = ET.parse(xml_out)
     root = tree.getroot()
 
@@ -172,11 +177,15 @@ def run_simulation(run_id):
         stop_time = float(ti.attrib["stopTime"])
         time_loss = float(ti.attrib["timeLoss"])
 
-        # Nowe metryki: czas po wyjeździe z parkingu
+        # CZAS WYJAZDU Z PARKINGU
         depart_after = depart + stop_time
+        # CZAS POTRZEBNY NA WYJECHANIE PO OPUSZCZENIU MIEJSCA PARKINGOWEGO
         after_parking_times.append(arrival - depart_after)
+        # PIERWSZY ODJAZ Z PARKINGU
         first_depart_after_parking = min(first_depart_after_parking, depart_after)
+        # KONIEC SYMULKI
         last_arrival = max(last_arrival, arrival)
+        # POSTOJE I INNE PRZESZKODY
         losses.append(time_loss)
 
     return {
@@ -200,4 +209,3 @@ if __name__ == "__main__":
         writer = csv.DictWriter(f, fieldnames=keys)
         writer.writeheader()
         writer.writerows(all_runs)
-    print("Wszystkie symulacje zakończone. Wyniki w sim_results/summary.csv")
